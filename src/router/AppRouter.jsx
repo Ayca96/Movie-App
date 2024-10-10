3aad7061ce3bf28097b39e305312a4ff070284d5
Ayca96 <aycakubra1@gmail.com>
2024-10-10 20:14:37 +0200

    "Added AppRouter and PrivateRouter components with routes configuration"

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from "../components/Navbar";
+import { Route, Routes } from 'react-router-dom';
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Main from "../pages/Main";
+import MovieDetail from "../pages/MovieDetail";
+import PrivateRouter from "./PrivateRouter";
+// toastContainer de Browser da görünsün diye browserRouter sarmalını üst component olan indexedDB.js de yaptık
+const AppRouter = () => {
+  return (
+    <>
+<Navbar/>
+
+<Routes>
+
+  <Route path="/" element={<Main/>}/>
+  <Route path="/login" element={<Login/>}/>
+  <Route path="/register" element={<Register/>}/>
+
+<Route path="/details/:id" element={<PrivateRouter/>}>
+
+
+<Route path="" element={<MovieDetail/>} />
+
+</Route>
+
+</Routes>
+
+
+    </>
+  )
+}
+
+export default AppRouter
