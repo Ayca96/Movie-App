95ab8d03bdfc9f8269f3172d1cdc7da57d374fad
Ayca96 <aycakubra1@gmail.com>
2024-10-28 14:06:34 +0100

    "Added notfoundImage CSS class and updated NotFound component to use it with an image"

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Foto from "../assets/icons/404-error-found-cat-is-sitting-holding-plug-from-outlet_626340-65.avif"
+import "../App.css"
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='notfoundImage'>
+      <img src={Foto} alt="" width="100%" />
+    </div>
   )
 }
 
