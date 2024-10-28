f001b1aaa8a81fa0a744a40e4b3c239e688fd36e
Ayca96 <aycakubra1@gmail.com>
2024-10-28 15:16:57 +0100

    "Refactored AppRouter component: reformatted code, removed unnecessary whitespace, and rearranged route definitions."

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,39 +1,32 @@
-import React from 'react'
+import React from "react";
 import Navbar from "../components/Navbar";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Main from "../pages/Main";
 import MovieDetail from "../pages/MovieDetail";
 import PrivateRouter from "./PrivateRouter";
-import NotFound from '../pages/NotFound';
+import NotFound from "../pages/NotFound";
 // toastContainer de Browser da görünsün diye browserRouter sarmalını üst component olan indexedDB.js de yaptık
 const AppRouter = () => {
   return (
     <>
-<Navbar/>
+      <Navbar />
 
-<Routes>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/page/:pageNumber" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-  <Route path="/" element={<Main/>}/>
-  <Route path="/page/:pageNumber" element={<Main/>} />
-  <Route path="/login" element={<Login/>}/>
-  <Route path="/register" element={<Register/>}/>
+        <Route path="/details/:id" element={<PrivateRouter />}>
+          <Route path="" element={<MovieDetail />} />
+        </Route>
 
-<Route path="/page/:pageNumber/details/:id" element={<PrivateRouter/>}>
-
-
-<Route path="" element={<MovieDetail/>} />
-
-</Route>
-
-<Route path="*" element={<NotFound />} />
-
-</Routes>
-
-
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
